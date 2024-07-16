7bde3cc6d8d7cc5b7668bcb82489c92e554a1192
Sergei Smirnov <sasmirnov87@gmail.com>
2024-07-16 12:59:29 +0300

    скелет MailForm

---
+++ src/components/MailForm/MailForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Grid, Paper, Card, Box, Typography, Link, Divider } from '@mui/material';
+import { Form } from '../Form';
+/* import { Modal } from '../Modal'; */
+
+export function MailForm() {
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={12} md={9}>
+                <Paper elevation={1} component={Card}>
+                    <Box p={3}>
+                        <Typography variant="h6" gutterBottom>
+                            Change your private information
+                        </Typography>
+                        <Typography variant="subtitle2">
+                            Please read our{' '}
+                            <Link href="https://www.google.com/" underline="none">
+                                terms of use
+                            </Link>{' '}
+                            to be informed how we manage your private data.
+                        </Typography>
+                        <Box my={2}>
+                            <Divider />
+                        </Box>
+                        <Form />
+                    </Box>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MailForm } from './components';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>Mail Form</React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <MailForm />
+    </React.StrictMode>
+);
